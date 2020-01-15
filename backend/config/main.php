3bff677716063d40470bb94e77e0cf81bb379d6a
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-01-15 13:09:46 +0200

    Add aliases for bower.

--- backend/config/main.php
+++ backend/config/main.php
@@ -13,6 +13,10 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm' => '@vendor/npm-asset',
+    ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
